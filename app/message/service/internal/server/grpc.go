28e43a6900874ae6dfe5c5f464e141e3608f62c3
intyou <123916296+intyouss@users.noreply.github.com>
2023-08-28 12:52:02 +0800

    Feat: Add full message service (#33)

---
+++ app/message/service/internal/server/grpc.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	v1 "github.com/toomanysource/atreus/api/message/service/v1"
+	"github.com/toomanysource/atreus/app/message/service/internal/conf"
+	"github.com/toomanysource/atreus/app/message/service/internal/service"
+
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+)
+
+// NewGRPCServer new a gRPC server.
+func NewGRPCServer(c *conf.Server, service *service.MessageService, logger log.Logger) *grpc.Server {
+	opts := []grpc.ServerOption{
+		grpc.Middleware(
+			recovery.Recovery(),
+			logging.Server(logger),
+		),
+	}
+	if c.Grpc.Network != "" {
+		opts = append(opts, grpc.Network(c.Grpc.Network))
+	}
+	if c.Grpc.Addr != "" {
+		opts = append(opts, grpc.Address(c.Grpc.Addr))
+	}
+	if c.Grpc.Timeout != nil {
+		opts = append(opts, grpc.Timeout(c.Grpc.Timeout.AsDuration()))
+	}
+	srv := grpc.NewServer(opts...)
+	v1.RegisterMessageServiceServer(srv, service)
+	return srv
+}
