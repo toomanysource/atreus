d372e3f5219c900849fb4dd49d672b4e3add8832
intyouss <123916296+intyouss@users.noreply.github.com>
2023-08-31 16:20:35 +0800

    feat: Adding a reminder message

--- pkg/kafkaX/writer.go
+++ pkg/kafkaX/writer.go
@@ -4,13 +4,21 @@ import (
 	"context"
 	"strconv"
 
+	"github.com/go-kratos/kratos/v2/log"
+
 	"github.com/segmentio/kafka-go"
 )
 
 func Update(writer *kafka.Writer, id uint32, num int32) error {
-	return writer.WriteMessages(context.TODO(),
+	err := writer.WriteMessages(context.TODO(),
 		kafka.Message{
-			Key:   []byte(strconv.Itoa(int(id))),
-			Value: []byte(strconv.Itoa(int(num))),
+			Partition: 0,
+			Key:       []byte(strconv.Itoa(int(id))),
+			Value:     []byte(strconv.Itoa(int(num))),
 		})
+	if err != nil {
+		return err
+	}
+	log.Infof("update message success, id: %v, num: %v", id, num)
+	return nil
 }
