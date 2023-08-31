bc5683e172f08028dbcd8f7d204a43050e4feb9c
Dyamidsteve <2857199455@qq.com>
2023-08-31 17:30:29 +0800

    make format

--- app/message/service/cmd/wire_gen.go
+++ app/message/service/cmd/wire_gen.go
@@ -9,14 +9,13 @@ package main
 import (
 	"github.com/go-kratos/kratos/v2"
 	"github.com/go-kratos/kratos/v2/log"
+
 	"github.com/toomanysource/atreus/app/message/service/internal/biz"
 	"github.com/toomanysource/atreus/app/message/service/internal/conf"
 	"github.com/toomanysource/atreus/app/message/service/internal/data"
 	"github.com/toomanysource/atreus/app/message/service/internal/server"
 	"github.com/toomanysource/atreus/app/message/service/internal/service"
-)
 
-import (
 	_ "go.uber.org/automaxprocs"
 )
 
