6ce7ddf77d6bb536dd2997418ab7de4105884af7
intyouss <123916296+intyouss@users.noreply.github.com>
2023-09-04 20:01:05 +0800

    fix: Organize code and standardize error formatting

--- app/favorite/service/internal/data/data.go
+++ app/favorite/service/internal/data/data.go
@@ -33,9 +33,8 @@ type Data struct {
 }
 
 func NewData(db *gorm.DB, cache *redis.Client, kfk KfkWriter, logger log.Logger) (*Data, func(), error) {
-	logHelper := log.NewHelper(log.With(logger, "module", "data/favorite"))
-	// 并发关闭所有数据库连接，后期根据Redis与Mysql是否数据同步修改
-	// MySQL 会自动关闭连接，但是 Redis 不会，所以需要手动关闭
+	logHelper := log.NewHelper(log.With(logger, "module", "data/data"))
+	// 并发关闭所有数据库连接
 	cleanup := func() {
 		var wg sync.WaitGroup
 		wg.Add(1)
@@ -46,33 +45,33 @@ func NewData(db *gorm.DB, cache *redis.Client, kfk KfkWriter, logger log.Logger)
 				return
 			}
 			if err = cache.Close(); err != nil {
-				logHelper.Errorf("Redis connection closure failed, err: %w", err)
+				logHelper.Errorf("redis connection closure failed, err: %w", err)
 			}
-			logHelper.Info("Successfully close the Redis connection")
+			logHelper.Info("successfully close the Redis connection")
 		}()
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			if err := kfk.Favored.Close(); err != nil {
-				logHelper.Errorf("Kafka connection closure failed, err: %w", err)
+				logHelper.Errorf("kafka connection closure failed, err: %w", err)
 			}
 		}()
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			if err := kfk.Favorite.Close(); err != nil {
-				logHelper.Errorf("Kafka connection closure failed, err: %w", err)
+				logHelper.Errorf("kafka connection closure failed, err: %w", err)
 			}
 		}()
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			if err := kfk.videoFavorite.Close(); err != nil {
-				logHelper.Errorf("Kafka connection closure failed, err: %w", err)
+				logHelper.Errorf("kafka connection closure failed, err: %w", err)
 			}
 		}()
 		wg.Wait()
-		logHelper.Info("Successfully close the Kafka connection")
+		logHelper.Info("successfully close the Kafka connection")
 	}
 
 	data := &Data{
@@ -86,21 +85,21 @@ func NewData(db *gorm.DB, cache *redis.Client, kfk KfkWriter, logger log.Logger)
 
 // NewMysqlConn mysql数据库连接
 func NewMysqlConn(c *conf.Data, l log.Logger) *gorm.DB {
-	logs := log.NewHelper(log.With(l, "module", "data/mysql"))
+	logs := log.NewHelper(log.With(l, "module", "data/data/mysql"))
 	db, err := gorm.Open(mysql.Open(c.Mysql.Dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
 	if err != nil {
-		logs.Fatalf("Database connection failure, err : %v", err)
+		logs.Fatalf("database connection failure, err : %v", err)
 	}
 	InitDB(db)
-	logs.Info("Database enabled successfully!")
-	return db.Model(&Favorite{})
+	logs.Info("database enabled successfully!")
+	return db
 }
 
 // NewRedisConn Redis数据库连接, 并发开启连接提高速率
 func NewRedisConn(c *conf.Data, l log.Logger) (cacheClient *redis.Client) {
-	logs := log.NewHelper(log.With(l, "module", "data/redis"))
+	logs := log.NewHelper(log.With(l, "module", "data/data/redis"))
 	// 初始化点赞数Redis客户端
 	cache := redis.NewClient(&redis.Options{
 		DB:           int(c.Redis.FavoriteDb),
@@ -113,13 +112,14 @@ func NewRedisConn(c *conf.Data, l log.Logger) (cacheClient *redis.Client) {
 	// ping Redis客户端，判断连接是否存在
 	_, err := cache.Ping(context.Background()).Result()
 	if err != nil {
-		logs.Fatalf("Redis database connection failure, err : %v", err)
+		logs.Fatalf("redis database connection failure, err : %v", err)
 	}
-	logs.Info("Cache enabled successfully!")
+	logs.Info("cache enabled successfully")
 	return cache
 }
 
-func NewKafkaWriter(c *conf.Data) KfkWriter {
+func NewKafkaWriter(c *conf.Data, l log.Logger) KfkWriter {
+	logs := log.NewHelper(log.With(l, "module", "data/data/kafka"))
 	writer := func(topic string) *kafka.Writer {
 		return &kafka.Writer{
 			Addr:                   kafka.TCP(c.Kafka.Addr),
@@ -130,6 +130,7 @@ func NewKafkaWriter(c *conf.Data) KfkWriter {
 			AllowAutoTopicCreation: true,
 		}
 	}
+	logs.Info("kafka enabled successfully")
 	return KfkWriter{
 		Favorite:      writer(c.Kafka.FavoriteTopic),
 		Favored:       writer(c.Kafka.FavoredTopic),
@@ -140,6 +141,6 @@ func NewKafkaWriter(c *conf.Data) KfkWriter {
 // InitDB 创建User数据表，并自动迁移
 func InitDB(db *gorm.DB) {
 	if err := db.AutoMigrate(&Favorite{}); err != nil {
-		log.Fatalf("Database initialization error, err : %v", err)
+		log.Fatalf("database initialization error, err : %v", err)
 	}
 }
